0b9b23c4be8a49b13d2e2ecefed16fce2cb87c24
sanjay28 <sanjaysanju02006@gmail.com>
2026-06-30 19:55:12 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left <= right) {
+            if (!isalnum(s[left])) {
+                left++;
+                continue;
+            }
+            if (!isalnum(s[right])) {
+                right--;
+                continue;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return false;
+    }
+};
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         // int n = s.size();
+//         string t = "";
+//         for(char ch: s){
+//           if(isalnum(ch)){
+//             t += tolower(ch);
+//           }
+//         }
+//         string reversestr = t;
+//         reverse(reversestr.begin(), reversestr.end());
+
+//         return t == reversestr;
+//     }
+// };
