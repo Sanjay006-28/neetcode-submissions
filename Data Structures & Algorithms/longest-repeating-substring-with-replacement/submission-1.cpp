f465ae07dd77c49e6253e66234ea51cddefd654c
sanjay28 <sanjaysanju02006@gmail.com>
2026-07-10 20:30:35 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int l = 0;
+        int maxlen = 0;
+        int maxfre = 0;
+        unordered_map<int, int> mpp;
+        for(int r = 0; r < n; r++){
+            mpp[s[r]]++;
+            maxfre = max(maxfre, mpp[s[r]]);
+            while(((r - l + 1)- maxfre) > k){
+                mpp[s[l]]--;
+                l++;
+            }
+            maxlen = max (maxlen, r - l + 1);
+        }
+        return maxlen;
+    }
+};
